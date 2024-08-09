55b283f3d9ec0f14b97fc2d70dffddcd427cca1f
guilhermereiiss <guiprogramador06@gmail.com>
2024-08-09 20:53:55 -0300

    Tp1 de Mobile-first UI com React Completo

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ export default function App() {
     return (
         <main>
             <a href="src/TP1/01/index.html">TP 1.1</a>
-            <a href="src/TP1/02/index.html">TP 1.2</a>
-            <a href="src/TP1/03/index.html">TP 1.3</a>
-            <a href="src/TP1/04/index.html">TP 1.4</a>
-            <a href="src/TP1/05/index.html">TP 1.5</a>
-            <a href="src/TP1/06/index.html">TP 1.6</a>
-            <a href="src/TP1/07/index.html">TP 1.7</a>
-            <a href="src/TP1/08/index.html">TP 1.8</a>
-            <a href="src/TP1/09/index.html">TP 1.9</a>
-            <a href="src/TP1/10/index.html">TP 1.10</a>
-            <a href="src/TP1/11/index.html">TP 1.11</a>
-            <a href="src/TP1/12/index.html">TP 1.12</a>
-            <a href="src/TP1/13/index.html">TP 1.13</a>
-            <a href="src/TP1/14/index.html">TP 1.14</a>
-            <a href="src/TP1/15/index.html">TP 1.15</a>
-            <a href="src/TP1/16/index.html">TP 1.16</a>
+            <a href="src/TP1/02/index2.html">TP 1.2</a>
+            <a href="src/TP1/03/index3.html">TP 1.3</a>
+            <a href="src/TP1/04/index4.html">TP 1.4</a>
+            <a href="src/TP1/05/index5.html">TP 1.5</a>
+            <a href="src/TP1/06/index6.html">TP 1.6</a>
+            <a href="src/TP1/07/index7.html">TP 1.7</a>
+            <a href="src/TP1/08/index8.html">TP 1.8</a>
+            <a href="src/TP1/09/index9.html">TP 1.9</a>
+            <a href="src/TP1/10/index10.html">TP 1.10</a>
+            <a href="src/TP1/11/index11.html">TP 1.11</a>
+            <a href="src/TP1/12/index12.html">TP 1.12</a>
+            <a href="src/TP1/13/index13.html">TP 1.13</a>
+            <a href="src/TP1/14/index14.html">TP 1.14</a>
+            <a href="src/TP1/15/index15.html">TP 1.15</a>
+            <a href="src/TP1/16/index16.html">TP 1.16</a>
         </main>
     )
 }
